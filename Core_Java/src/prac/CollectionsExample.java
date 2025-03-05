ba87e969b3b967801b14069b59b3fe042b5a5ac7
unknown <aayush.tiwari@nucleusteq.com>
2025-03-05 14:23:44 +0530

    Added Java Core project

---
+++ Core_Java/src/prac/CollectionsExample.java
@@ -0,0 +1,17 @@
+package prac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsExample {
+
+	public static void main(String[] args) {
+		System.out.println("Start of the program");
+		List<String> arrList = new ArrayList<>();
+		arrList.add("Aayush");
+		arrList.add("Lalit");
+		arrList.add("Nachiketa");
+		System.out.println("The items in array list is -> " + arrList);
+	}
+
+}
