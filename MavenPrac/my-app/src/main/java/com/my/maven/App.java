197cb5e98a7993b1dc4585c2200d74dd30dcd054
unknown <aayush.tiwari@nucleusteq.com>
2025-03-10 16:44:28 +0530

    Implemented maven cmd

---
+++ MavenPrac/my-app/src/main/java/com/my/maven/App.java
@@ -0,0 +1,16 @@
+package com.my.maven;
+
+import com.my.maven.helper.Helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Helper.help();
+    }
+}
---
+++ MavenPrac/my-app/src/main/java/com/my/maven/helper/Helper.java
@@ -0,0 +1,7 @@
+package com.my.maven.helper;
+
+public class Helper {
+	public static void help() {
+		System.out.println("I am the helper method in helper package");
+	}
+}
