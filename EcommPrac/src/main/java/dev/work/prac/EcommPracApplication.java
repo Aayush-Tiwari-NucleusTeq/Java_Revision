bf0d519544e92e3766d72c94024f5c27a0bdc1f2
unknown <aayush.tiwari@nucleusteq.com>
2025-03-17 15:10:49 +0530

    Updated

--- EcommPrac/src/main/java/dev/work/prac/EcommPracApplication.java
+++ EcommPrac/src/main/java/dev/work/prac/EcommPracApplication.java
@@ -3,13 +3,13 @@ package dev.work.prac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dev.work.prac.entities.User;
 
 @SpringBootApplication
 public class EcommPracApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommPracApplication.class, args);
+		
 	}
 
 }
