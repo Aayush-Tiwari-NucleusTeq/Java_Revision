af7b10df17498e46cc14546c9cdf568a72c747d9
unknown <aayush.tiwari@nucleusteq.com>
2025-03-20 12:29:26 +0530

    Updates something

--- MavenPrac/my-app/src/main/java/com/my/maven/App.java
+++ MavenPrac/my-app/src/main/java/com/my/maven/App.java
@@ -11,6 +11,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Helper.help();
+//        Helper.help();
+        System.out.println("String hashcodes");
+        String str = "Aayush";
+        System.out.println(str.hashCode());
+        str = str.concat(" Tiwari");
+        System.out.println(str.hashCode());
+        System.out.println("\n String Builder hashcodes");
+        StringBuilder str2 = new StringBuilder("Lalit");
+        System.out.println(str2.hashCode());
+        str2 = str2.append(" Patel");
+        System.out.println(str2.hashCode());
     }
 }
