580c493bdc9900033fe61e655fedb36d409e6f62
unknown <aayush.tiwari@nucleusteq.com>
2025-05-02 16:19:07 +0530

    Added some files

--- Core_Java/src/dsa/array/KadanesAlgo.java
+++ Core_Java/src/dsa/array/KadanesAlgo.java
@@ -29,11 +29,29 @@ public class KadanesAlgo {
 		
 		return maxele;
 	}
+	
+	public static int optimal(int[] arr) {
+		int maxele = Integer.MIN_VALUE;
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum += arr[i];
+			
+			if(sum > maxele) {
+				maxele = sum;
+			}
+			
+			if(sum < 0) {
+				sum = 0;
+			}
+		}
+		return maxele;
+	}
 
 	public static void main(String[] args) {
-		int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+		int[] arr = {-1,-9, 1, 2, 3, 4};
 //		int bruteForce = bruteForce(arr);
-		int better = better(arr);
-		System.out.println("The maximum sum from the array is - " + better);
+//		int better = better(arr);
+		int optimal = optimal(arr);
+		System.out.println("The maximum sum from the array is - " + optimal);
 	}
 }
---
+++ Core_Java/src/dsa/array/MajorityElement.java
@@ -0,0 +1,14 @@
+package dsa.array;
+
+public class MajorityElement {
+
+	public static void bruteForce(int[] arr) {
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {2, 2, 1, 1, 1, 2, 2};
+		
+	}
+
+}
